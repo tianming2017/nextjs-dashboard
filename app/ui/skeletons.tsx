f336f4ed5a3187a90dc0ef2822f5cb534d4b3352
huhaoxiong <hhx17181@mail.haitian.com>
2024-12-09 16:49:01 +0800

    deom app

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -20,12 +20,12 @@ export function CardSkeleton() {
 
 export function CardsSkeleton() {
   return (
-    <>
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
       <CardSkeleton />
-    </>
+    </div>
   );
 }
 
